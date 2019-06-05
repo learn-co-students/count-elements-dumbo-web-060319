ac6dc44b7f54fe106a14011701f48391b52ee05a
Alexander Schelchere <ascellie@gmail.com>
2019-06-05 09:01:33 -0400

    count elements - complete

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require "pry"
+
 def count_elements(array)
-  # code goes here
+  # Hash to store the count
+  array_item_count = {}
+  array.each do |item|
+    if array_item_count.has_key? item
+      array_item_count[item] += 1
+    else
+      array_item_count[item] = 1
+    end
+  end
+  array_item_count
 end
  
